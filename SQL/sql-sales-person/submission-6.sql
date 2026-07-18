541a1c20d8e11e0a88323007fb26b8b8db1c580c
sekinat <sekinat95@gmail.com>
2026-07-18 22:58:03 +0200

    Add: sql-sales-person - submission-6

---
+++ SQL/sql-sales-person/submission-6.sql
@@ -0,0 +1,14 @@
+--select name
+--from sales_person
+--where sales_id not in (
+--    select sales_id from orders o
+--    join company c on o.com_id = c.com_id
+--    where c.name = 'CRIMSON'
+--);
+
+select name 
+from sales_person
+where sales_id not in (
+    select sales_id from orders
+    where com_id = (select com_id from company where name='CRIMSON')
+);
