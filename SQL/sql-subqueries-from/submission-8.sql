d74246901d59a5418dc84431b1de545d2d2da272
sekinat <sekinat95@gmail.com>
2026-07-18 16:08:36 +0200

    Add: sql-subqueries-from - submission-8

---
+++ SQL/sql-subqueries-from/submission-8.sql
@@ -0,0 +1,34 @@
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name TEXT,
+    salary INTEGER,
+    department TEXT
+);
+
+INSERT INTO employees (name, salary, department) VALUES
+  ('Alice', 50000, 'marketing'),
+  ('Bob', 60000, 'marketing'),
+  ('Charlie', 55000, 'marketing'),
+  ('David', 65000, 'marketing'),
+  ('Eve', 70000, 'finance'),
+  ('Frank', 52000, 'finance'),
+  ('Grace', 58000, 'finance'),
+  ('Hank', 62000, 'finance');
+-- Do not modify above this line. --
+
+--select name, salary
+--from employees
+--where department = 'marketing'
+--and salary < (select avg(salary) from employees where department = 'marketing')
+--order by salary asc;
+-----------------------------------------
+with avg_sal as (
+    select avg(salary) as avg_salary from employees where department='marketing'
+)
+select name, salary 
+from employees
+where department='marketing' and salary < (select avg_salary from avg_sal)
+order by salary asc;
+
+
+
