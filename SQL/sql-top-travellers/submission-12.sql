da11cc4a65b19ce2f6f3fa26eae2d354b95813e3
sekinat <sekinat95@gmail.com>
2026-07-18 22:21:02 +0200

    Add: sql-top-travellers - submission-12

---
+++ SQL/sql-top-travellers/submission-12.sql
@@ -0,0 +1,29 @@
+--with total_distance_per_user as (
+--   select name, coalesce(sum(distance), 0) as travelled_distance
+--  from users 
+--   left join rides on rides.user_id = users.id
+--   group by users.id, users.name
+--)
+--select name, travelled_distance
+--from total_distance_per_user
+--order by travelled_distance desc, name asc;
+
+--------------------------
+
+select u.name, coalesce(sum(r.distance), 0) as travelled_distance
+from users u 
+left join rides r on u.id = r.user_id
+group by u.id, u.name
+order by travelled_distance desc, name asc;
+
+
+
+
+
+
+
+
+
+
+
+
